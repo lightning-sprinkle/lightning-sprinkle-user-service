5649c66d9489a479874395fc322b56eff6db0554
Daan Middendorp <github-d@landgenoot.com>
2020-03-03 19:14:25 +0100

    Do organization check

--- app/server.py
+++ app/server.py
@@ -2,16 +2,19 @@
 import app.lnd as lnd
 import app.reward as reward
 import app.cert as cert
+import config
 from flask import Blueprint, escape, request
+from urllib.parse import urlparse
 
 # from sprinkle import app
 server = Blueprint('server', __name__)
 @server.route('/request-payment/<dest>')
 def pay(dest):
-  # amt = reward.get_current_reward()
-  # response = lnd.send_money(dest, amt)
-  # payment_hash = response.payment_hash.hex()
-  print(request.headers)
-  print(cert.isOrganization(request.headers.get('Referer')))
-  return ''
-  # return f'Payment hash: {escape(payment_hash)}'
+  hostname = urlparse(request.headers.get('Referer')).netloc
+  if not config.organization_only or cert.isOrganization(hostname):
+    amt = reward.get_current_reward()
+    response = lnd.send_money(dest, amt)
+    payment_hash = response.payment_hash.hex()
+    return f'Payment hash: {escape(payment_hash)}'
+  else:
+    return f'{escape(hostname)} is not an organization'
--- config.py
+++ config.py
@@ -6,6 +6,9 @@ Application configuration
 # The maximum reward per hour in satoshis
 max_hourly_reward = 40
 
+# Only reward hostnams with a valid OV or EV certificate.
+organization_only = True
+
 # Hostnames that will never get a reward
 blacklist = [
   'example.com'
